6f0b1678c54da8b29cbf9fbc7b1b49104f14494e
neeraj upreti <neerajupreti@neerajs-MacBook-Air.local>
2025-11-09 11:19:12 +0530

    update xyz

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  build: {
-    sourcemap: false,
-    minify: "esbuild"
-  }
-}
-
+export default defineConfig({
+  plugins: [react()],
+})
