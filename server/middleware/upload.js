a7d8f77cbf4b5448e02dd64908b5daa67c47f510
neeraj upreti <neerajupreti@neerajs-MacBook-Air.local>
2025-12-08 21:31:00 +0530

    few fixes

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,14 +1,14 @@
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "tmp_uploads/");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "tmp_uploads/");
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     },
+// });
 
 const upload = multer({ storage: multer.memoryStorage() });
 
