221122ca364b2afec3baecc9744e299ecfd948a0
neeraj upreti <neerajupreti@neerajs-MacBook-Air.local>
2025-12-09 16:31:10 +0530

    message wala

--- server/models/chat.model.js
+++ server/models/chat.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
-    lastMessage: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Message",
-        default: null
+    lastMessage: {                      // 👇 change type here
+        text: String,
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        timestamp: Date
     }
 }, { timestamps: true });
 
