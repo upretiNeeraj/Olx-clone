d0a3c24dfa7011498207d6b978fb2d4bd10367c1
neeraj upreti <neerajupreti@neerajs-MacBook-Air.local>
2025-12-09 16:33:05 +0530

    message wala

--- server/models/chat.model.js
+++ server/models/chat.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
-    lastMessage: {                      // 👇 change type here
-        text: String,
+    lastMessage: {                           // 🔥 now it accepts an object
+        text: { type: String, default: "" },
         sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        timestamp: Date
+        timestamp: { type: Date }
     }
 }, { timestamps: true });
 
