8dcda044d503d5532c2b2a88f44a787d3463bd62
neeraj upreti <neerajupreti@neerajs-MacBook-Air.local>
2025-12-09 17:00:19 +0530

    css only

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: "https://olx-clone-jade-nine.vercel.app",
+        origin: ["https://olx-clone-jade-nine.vercel.app", "*"],
         methods: ["GET", "POST"],
         credentials: true,
         transports: ["websocket"]
